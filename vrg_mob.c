bfbd6b48400d632c7b247b7c2a9838080b29969b
assasin007-24 <assasin.007.24@gmail.com>
2025-01-31 11:44:21 -0800

    .

--- vrg_mob.c
+++ vrg_mob.c
@@ -3,4 +3,10 @@ void main(void)
 {
     printf("Numarul 0.1234 este afisat in formatul %g\n", 0.1234);
     printf("Numarul 0.00001234 este afisat in formatul %g\n", 0.00001234);
+    int valoare = 5;
+
+    printf("%01\n", valoare);
+    printf("%02\n", valoare);
+    printf("%03\n", valoare);
+    printf("%04\n", valoare);
 }
