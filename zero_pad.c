42ee4ffe0a54c60587c749126ad56b5b75ca98be
assasin007-24 <assasin.007.24@gmail.com>
2025-01-31 11:34:27 -0800

    .

---
+++ zero_pad.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main(void)
+{
+    int valoare = 5;
+
+    printf("%01\n", valoare);
+    printf("%02\n", valoare);
+    printf("%03\n", valoare);
+    printf("%04\n", valoare);
+}
